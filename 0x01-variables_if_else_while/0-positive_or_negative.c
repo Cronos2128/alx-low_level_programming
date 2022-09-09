8be303337adfbacfec2768d3ec5c8a439b446be9
Cronos2128 <nebyqu@gmail.com>
2022-09-09 03:25:15 -0700

    This program will assign a random number to the variable n each time it is executed. Complete the source code in order to print whether the number stored in the variable n is positive or negative.

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,11 +5,10 @@
 /**
  *  main - entry point
  *
- *  Description: Prints a number and whether it is positive, negative or zero.
- *
- *   Return : 0
+ *  Description: Prints a number and it's last digit along with whether its
+ *  last digit is 0, greater than 5, or less than 6 and not 0.
+ *  Return: 0
  */
-
 int main(void)
 {
 	int n;
