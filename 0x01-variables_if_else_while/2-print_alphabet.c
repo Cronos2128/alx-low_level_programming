f02d3d34843cee796544253dfa28dd65409feb09
Cronos2128 <nebyqu@gmail.com>
2022-09-09 03:20:33 -0700

    Write a program that prints the alphabet in lowercase, followed by a new line.

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,21 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 
 /**
- * main - entry point
+ *  main - entry point
  *
- * description : Write a program that prints the alphabet in lowercase
- *
- * return : 0
+ *  Description: Prints a number and it's last digit along with whether its
+ *  last digit is 0, greater than 5, or less than 6 and not 0.
+ *  Return: 0
  */
- int main(void)
+int main(void)
 {
-	char c;
+	int c;
 
-	for (c = 'a'; c <= 'z'; c++)
-	{
-		putchar(c);
-	}
-	putchr('\n');
+	for (c = 0; c < 26; ++c)
+		putchar('a' + c);
+
+	putchar('\n');
+
 	return (0);
 }
-
